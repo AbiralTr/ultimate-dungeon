10ed5fc0f44ae5265969a376b2774a3802cf2704
AbiralTr <abiraltuladhar22@gmail.com>
2026-01-11 12:23:56 -0700

    Small update frontend,CSS updated

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,9 @@ app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(process.cwd(), "views"));
 
+app.use(express.static("public"));
+
+
 app.get("/", (req, res) => {res.render("home")});
 app.get("/login", (req, res) => {res.render("login")});
 app.get("/register", (req, res) => {res.render("register")});
